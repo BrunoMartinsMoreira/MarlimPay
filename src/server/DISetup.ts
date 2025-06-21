08a061b6b172f238dc6563480edab8ecc234ee6a
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-06-21 16:47:17 -0300

    feat: configura DI de TransactionsRepository

--- src/server/DISetup.ts
+++ src/server/DISetup.ts
@@ -1,6 +1,23 @@
 import { container } from 'tsyringe';
-import { IUserRepository, UserRepository } from '../repositories';
+import {
+  IUserRepository,
+  UserRepository,
+  ITransactionRepository,
+  TransactionRepository,
+} from '../repositories';
+
+export const TOKENS = {
+  UserRepository: Symbol('UserRepository'),
+  TransactionRepository: Symbol('TransactionRepository'),
+} as const;
 
 export function DISetup() {
-  container.register<IUserRepository>('UserRepository', UserRepository);
+  container.registerSingleton<IUserRepository>(
+    TOKENS.UserRepository,
+    UserRepository,
+  );
+  container.register<ITransactionRepository>(
+    TOKENS.TransactionRepository,
+    TransactionRepository,
+  );
 }
