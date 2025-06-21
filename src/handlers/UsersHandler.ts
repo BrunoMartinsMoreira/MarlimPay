49642b0648607c4d9539015cc7a91f3aa43ff879
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-06-21 01:39:37 -0300

    feat: adiciona injecao de dependencias pelo tsyringe

--- src/handlers/UsersHandler.ts
+++ src/handlers/UsersHandler.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { UserService } from '../services/UserService';
 import {
   CreateUserDTO,
   createUserSchema,
@@ -7,30 +6,29 @@ import {
   updateUserSchema,
   userParamsSchema,
 } from '../schemas';
+import { container, injectable } from 'tsyringe';
+import { UserService } from '../services';
 
+@injectable()
 export class UsersHandler {
-  private readonly userService: UserService;
+  private readonly userService = container.resolve(UserService);
 
-  constructor() {
-    this.userService = new UserService();
-  }
-
-  createUser(req: Request, res: Response) {
+  async createUser(req: Request, res: Response) {
     const dto: CreateUserDTO = createUserSchema.parse(req.body);
-    const user = this.userService.createUser(dto);
+    const user = await this.userService.createUser(dto);
     res.status(201).send(user);
   }
 
-  updateUser(req: Request, res: Response) {
+  async updateUser(req: Request, res: Response) {
     const dto: UpdateUserDTO = updateUserSchema.parse(req.body);
-    const { user_id } = userParamsSchema.parse({ user_id: req.params.id });
-    const user = this.userService.updateUser(dto, user_id);
+    const { user_id } = userParamsSchema.parse({ user_id: req.params.user_id });
+    const user = await this.userService.updateUser(dto, user_id);
     res.status(201).send(user);
   }
 
-  getUser(req: Request, res: Response) {
-    const { user_id } = userParamsSchema.parse({ user_id: req.params.id });
-    const user = this.userService.getUser(user_id);
+  async getUser(req: Request, res: Response) {
+    const { user_id } = userParamsSchema.parse({ user_id: req.params.user_id });
+    const user = await this.userService.findUser(user_id);
     res.status(200).send(user);
   }
 }
