ae012d9faabbf361aa1578d9c47df2a21655ba92
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-06-21 22:06:40 -0300

    feat: altera estrutura do projeto para utilizar cloud functions

--- src/errors/ApplicationError.ts
+++
@@ -1,9 +0,0 @@
-export class ApplicationError extends Error {
-  public status: number;
-
-  constructor(message: string, status: number) {
-    super(message);
-    this.name = 'ApplicationError';
-    this.status = status;
-  }
-}
--- src/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Contracts/IUserRepository';
-export * from './Contracts/ITransactioRepository';
-export * from './Contracts/IWebhookEventsRepository';
-export * from './UserRepository';
-export * from './TransactionRepository';
-export * from './WebhookEventsRepository';
