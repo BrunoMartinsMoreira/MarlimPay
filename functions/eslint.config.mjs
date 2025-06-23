ab90c122b347c54e14e2c39a3e9c1004a5982468
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-06-23 01:11:34 -0300

    chore: corrige regras eslint para ignorar pasta public

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -7,7 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['lib'],
+    ignores: ['lib', 'public'],
   },
   {
     rules: {
